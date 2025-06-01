d4aed03f6cc8a13ffb2e16fea4bdb5755e008b74
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-06-01 10:54:05 +0300

    Snippets

---
+++ packages/setup-whitespace-cleanup-mode.el
@@ -0,0 +1,12 @@
+;; setup-whitespace-cleanup-mode.el
+;;
+;; A minor mode that will intelligently call whitespace-cleanup before buffers
+;; are saved.
+
+(use-package whitespace-cleanup-mode
+  :diminish whitespace-cleanup-mode
+  :defer 2
+  :init
+  (global-whitespace-cleanup-mode))
+
+(provide 'setup-whitespace-cleanup-mode)
