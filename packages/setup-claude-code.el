baf8f2700cf654d94ea29f5c05ccacf72f4272d9
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-06-14 12:12:25 +0300

    Add claude-code-send-shift-tab

--- packages/setup-claude-code.el
+++ packages/setup-claude-code.el
@@ -8,5 +8,22 @@
                    :files ("*.el" (:exclude "demo.gif")))
   :bind-keymap
   ("C-c c" . claude-code-command-map)
+  :bind (:map claude-code-command-map
+              ("Y" . claude-code-send-shift-tab))
   :config
   (claude-code-mode))
+
+
+(defun claude-code-send-shift-tab ()
+  "Send <shift-tab> to the Claude Code REPL.
+
+  This is useful for saying \"Always allow\" when Claude asks for
+  confirmation
+  without having to switch to the REPL buffer."
+  (interactive)
+  (if-let ((claude-code-buffer
+            (claude-code--get-or-prompt-for-buffer)))
+      (with-current-buffer claude-code-buffer
+        (eat-term-send-string eat-terminal "\e[Z")
+        (display-buffer claude-code-buffer))
+    (claude-code--show-not-running-message)))
