92225765f9c2242cd76cec6f11641032e4bf3094
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-07-21 10:22:42 +0300

    Progress on notmuch setup

---
+++ packages/setup-notmuch.el
@@ -0,0 +1,237 @@
+;; setup-notmuch.el
+;; notmuch is a mail indexer and Mail User Agent (MUA). I use it to read and organize emails
+
+;; I install notmuch from the distro's repos because the CLI program is
+;; not dependent on Emacs. Though the package also includes notmuch.el
+;; which is what we use here (they are maintained by the same people).
+(use-package notmuch
+  :load-path "/opt/homebrew/Cellar/notmuch/0.39/share/emacs/site-lisp/notmuch/"
+  :defer t
+  :commands (notmuch notmuch-mua-new-mail))
+
+(autoload 'auth-source-search "auth-source")
+
+;;;###autoload
+(defun prot-common-auth-get-field (host prop)
+  "Find PROP in `auth-sources' for HOST entry."
+  (when-let* ((source (auth-source-search :host host)))
+    (if (eq prop :secret)
+        (funcall (plist-get (car source) prop))
+      (plist-get (flatten-list source) prop))))
+
+(use-package notmuch
+  :defer t
+  :config
+  (let ((prim (prot-common-auth-get-field "gmail-primary-smtp" :user))
+
+        ;;(sec (prot-common-auth-get-field "gmail-secondary-smtp" :user))
+        )
+    (setq notmuch-identities
+          (mapcar (lambda (str)
+                    (format "%s <%s>" user-full-name str))
+                  (list prim))
+          notmuch-fcc-dirs
+          `((,prim . "gmail/Sent")
+
+
+            ;;(,sec . "gmail/Sent")
+            ))))
+
+(use-package notmuch
+  :defer t
+  :config
+  (setq notmuch-show-logo nil
+        notmuch-column-control 1.0
+        notmuch-hello-auto-refresh t
+        notmuch-hello-recent-searches-max 20
+        notmuch-hello-thousands-separator ""
+        notmuch-hello-sections '(notmuch-hello-insert-saved-searches)
+        notmuch-show-all-tags-list t))
+
+(use-package notmuch
+  :defer t
+  :config
+  (setq notmuch-search-oldest-first nil)
+  (setq notmuch-search-result-format
+        '(("date" . "%12s  ")
+          ("count" . "%-7s  ")
+          ("authors" . "%-20s  ")
+          ("subject" . "%-80s  ")
+          ("tags" . "(%s)")))
+  (setq notmuch-tree-result-format
+        '(("date" . "%12s  ")
+          ("authors" . "%-20s  ")
+          ((("tree" . "%s")
+            ("subject" . "%s"))
+           . " %-80s  ")
+          ("tags" . "(%s)")))
+  (setq notmuch-search-line-faces
+        '(("unread" . notmuch-search-unread-face)
+          ;; ;; NOTE 2022-09-19: I disable this because I add a cosmeic
+          ;; ;; emoji via `notmuch-tag-formats'.  This way I do not get
+          ;; ;; an intense style which is very distracting when I filter
+          ;; ;; my mail to include this tag.
+          ;;
+          ;; ("flag" . notmuch-search-flagged-face)
+          ;;
+          ;; Using `italic' instead is just fine.  Though I also tried
+          ;; it without any face and I was okay with it.  The upside of
+          ;; having a face is that you can identify the message even
+          ;; when the window is split and you don't see the tags.
+          ("flag" . italic)))
+  (setq notmuch-show-empty-saved-searches t)
+  (setq notmuch-saved-searches
+        `(( :name "📥 inbox"
+            :query "tag:inbox"
+            :sort-order newest-first
+            :key ,(kbd "i"))
+          ( :name "💬 all unread (inbox)"
+            :query "tag:unread and tag:inbox"
+            :sort-order newest-first
+            :key ,(kbd "u")))))
+
+(use-package notmuch
+  :defer t
+  :config
+  (setq notmuch-archive-tags nil ; I do not archive email
+        notmuch-message-replied-tags '("+replied")
+        notmuch-message-forwarded-tags '("+forwarded")
+        notmuch-show-mark-read-tags '("-unread")
+        notmuch-draft-tags '("+draft")
+        notmuch-draft-folder "drafts"
+        notmuch-draft-save-plaintext 'ask)
+
+  ;; Also see `notmuch-tagging-keys' in the `prot-notmuch' section
+  ;; further below.
+  ;;
+  ;; All emoji are cosmetic.  The tags are just the text.
+  (setq notmuch-tag-formats
+        '(("unread" (propertize tag 'face 'notmuch-tag-unread))
+          ("flag" (propertize tag 'face 'notmuch-tag-flagged)
+           (concat tag "🚩")))
+        notmuch-tag-deleted-formats
+        '(("unread" (notmuch-apply-face bare-tag 'notmuch-tag-deleted)
+           (concat "👁️‍🗨️" tag))
+          (".*" (notmuch-apply-face tag 'notmuch-tag-deleted)
+           (concat "🚫" tag)))
+        notmuch-tag-added-formats
+        '(("del" (notmuch-apply-face tag 'notmuch-tag-added)
+           (concat "💥" tag))
+          (".*" (notmuch-apply-face tag 'notmuch-tag-added)
+           (concat "🏷️" tag)))))
+
+
+;;;; Email composition
+(use-package notmuch
+  :defer t
+  :config
+  (setq notmuch-mua-compose-in 'current-window)
+  (setq notmuch-mua-hidden-headers nil)
+  (setq notmuch-address-command 'internal)
+  (setq notmuch-address-use-company nil)
+  (setq notmuch-always-prompt-for-sender t)
+  (setq notmuch-mua-cite-function 'message-cite-original-without-signature)
+  (setq notmuch-mua-reply-insert-header-p-function 'notmuch-show-reply-insert-header-p-never)
+  (setq notmuch-mua-user-agent-function nil)
+  (setq notmuch-maildir-use-notmuch-insert t)
+  (setq notmuch-crypto-process-mime t)
+  (setq notmuch-crypto-get-keys-asynchronously t)
+  (setq notmuch-mua-attachment-regexp   ; see `notmuch-mua-send-hook'
+        (concat "\\b\\(attache\?ment\\|attached\\|attach\\|"
+                "pi[èe]ce\s+jointe?\\|"
+                "συνημμ[εέ]νο\\|επισυν[αά]πτω\\)\\b"))
+
+  (defun prot-notmuch-message-tab ()
+    "Override for `message-tab' to enforce header line check.
+More specifically, perform address completion when on a relevant header
+line, because `message-tab' sometimes (not sure when/how) fails to do
+that and instead tries to complete against dictionary entries."
+    (interactive nil message-mode)
+    (cond
+     ((save-excursion
+        (goto-char (line-beginning-position))
+        (looking-at notmuch-address-completion-headers-regexp))
+      (notmuch-address-expand-name)
+      ;; Completion was performed; nothing else to do.
+      nil)
+     (message-tab-body-function (funcall message-tab-body-function))
+     (t (funcall (or (lookup-key text-mode-map "\t")
+                     (lookup-key global-map "\t")
+                     'indent-relative)))))
+
+  (advice-add #'message-tab :override #'prot-notmuch-message-tab))
+
+(use-package notmuch
+  :defer t
+  :config
+  (setq notmuch-show-relative-dates t)
+  (setq notmuch-show-all-multipart/alternative-parts nil)
+  (setq notmuch-show-indent-messages-width 0)
+  (setq notmuch-show-indent-multipart nil)
+  (setq notmuch-show-part-button-default-action 'notmuch-show-view-part)
+  (setq notmuch-show-text/html-blocked-images ".") ; block everything
+  (setq notmuch-wash-wrap-lines-length 120)
+  (setq notmuch-unthreaded-show-out nil)
+  (setq notmuch-message-headers '("To" "Cc" "Subject" "Date"))
+  (setq notmuch-message-headers-visible t)
+
+  (let ((count most-positive-fixnum)) ; I don't like the buttonisation of long quotes
+    (setq notmuch-wash-citation-lines-prefix count
+          notmuch-wash-citation-lines-suffix count)))
+
+;;;; Hooks and key bindings
+(use-package notmuch
+  :hook
+  (notmuch-mua-send . notmuch-mua-attachment-check) ; also see `notmuch-mua-attachment-regexp'
+  (notmuch-show . (lambda () (setq-local header-line-format nil)))
+  :config
+  (remove-hook 'notmuch-show-hook #'notmuch-show-turn-on-visual-line-mode)
+  (remove-hook 'notmuch-search-hook #'notmuch-hl-line-mode) ; Check my `lin' package
+  :bind
+  ( :map global-map
+    ("C-c m" . notmuch)
+    ;; ("C-x m" . notmuch-mua-new-mail) ; override `compose-mail'
+    :map notmuch-search-mode-map ; I normally don't use the tree view, otherwise check `notmuch-tree-mode-map'
+    ("a" . nil) ; the default is too easy to hit accidentally and I do not archive stuff
+    ("A" . nil)
+    ("/" . notmuch-search-filter) ; alias for l
+    ("r" . notmuch-search-reply-to-thread) ; easier to reply to all by default
+    ("R" . notmuch-search-reply-to-thread-sender)
+    :map notmuch-show-mode-map
+    ("a" . nil) ; the default is too easy to hit accidentally and I do not archive stuff
+    ("A" . nil)
+    ("r" . notmuch-show-reply) ; easier to reply to all by default
+    ("R" . notmuch-show-reply-sender)
+    :map notmuch-hello-mode-map
+    ("C-<tab>" . nil)))
+
+(use-package prot-notmuch
+  :ensure nil
+  :after notmuch
+  :bind
+  ( :map notmuch-search-mode-map
+    ("D" . prot-notmuch-search-delete-thread)
+    ("S" . prot-notmuch-search-spam-thread)
+    ("g" . prot-notmuch-refresh-buffer)
+    :map notmuch-show-mode-map
+    ("D" . prot-notmuch-show-delete-message)
+    ("S" . prot-notmuch-show-spam-message)
+    :map notmuch-show-stash-map
+    ("S" . prot-notmuch-stash-sourcehut-link))
+  :config
+  ;; Those are for the actions that are available after pressing 'k'
+  ;; (`notmuch-tag-jump').  For direct actions, refer to the key
+  ;; bindings below.
+  (setq notmuch-tagging-keys
+        `((,(kbd "d") prot-notmuch-mark-delete-tags "💥 Mark for deletion")
+          (,(kbd "f") prot-notmuch-mark-flag-tags "🚩 Flag as important")
+          (,(kbd "s") prot-notmuch-mark-spam-tags "🔥 Mark as spam")
+          (,(kbd "r") ("-unread") "👁️‍🗨️ Mark as read")
+          (,(kbd "u") ("+unread") "🗨️ Mark as unread")))
+
+  ;; These emoji are purely cosmetic.  The tag remains the same: I
+  ;; would not like to input emoji for searching.
+  (add-to-list 'notmuch-tag-formats '("encrypted" (concat tag "🔒")))
+  (add-to-list 'notmuch-tag-formats '("attachment" (concat tag "📎")))
+  (add-to-list 'notmuch-tag-formats '("coach" (concat tag "🏆")))
+  (add-to-list 'notmuch-tag-formats '("package" (concat tag "🗂️"))))
---
+++ settings/prot-notmuch.el
@@ -0,0 +1,275 @@
+;;; prot-notmuch.el --- Tweaks for my notmuch.el configurations -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021-2025  Protesilaos Stavrou
+
+;; Author: Protesilaos Stavrou <info@protesilaos.com>
+;; URL: https://protesilaos.com/emacs/dotemacs
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "30.1"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; This covers my tweaks for notmuch.el that are meant for use in my
+;; Emacs setup: https://protesilaos.com/emacs/dotemacs.
+;;
+;; Remember that every piece of Elisp that I write is for my own
+;; educational and recreational purposes.  I am not a programmer and I
+;; do not recommend that you copy any of this if you are not certain of
+;; what it does.
+
+;;; Code:
+
+(require 'prot-common)
+(eval-when-compile (require 'cl-lib))
+
+(defgroup prot-notmuch ()
+  "Extensions for notmuch.el."
+  :group 'notmuch)
+
+(defcustom prot-notmuch-delete-tag "del"
+  "Single tag that applies to mail marked for deletion.
+This is used by `prot-notmuch-delete-mail'."
+  :type 'string
+  :group 'prot-notmuch)
+
+(defcustom prot-notmuch-mark-delete-tags
+  `(,(format "+%s" prot-notmuch-delete-tag) "-inbox" "-unread")
+  "List of tags to mark for deletion.
+To actually delete email, refer to `prot-notmuch-delete-mail'."
+  :type '(repeat string)
+  :group 'prot-notmuch)
+
+(defcustom prot-notmuch-mark-flag-tags '("+flag" "-unread")
+  "List of tags to mark as important (flagged).
+This gets the `notmuch-tag-flagged' face, if that is specified in
+`notmuch-tag-formats'."
+  :type '(repeat string)
+  :group 'prot-notmuch)
+
+(defcustom prot-notmuch-mark-spam-tags '("+spam" "-inbox" "-unread")
+  "List of tags to mark as spam."
+  :type '(repeat string)
+  :group 'prot-notmuch)
+
+;;;; Commands
+
+(autoload 'notmuch-interactive-region "notmuch")
+(autoload 'notmuch-tag-change-list "notmuch")
+(autoload 'notmuch-search-next-thread "notmuch")
+(autoload 'notmuch-search-tag "notmuch")
+
+(defmacro prot-notmuch-search-tag-thread (name tags)
+  "Produce NAME function parsing TAGS."
+  (declare (indent defun))
+  `(defun ,name (&optional untag beg end)
+     ,(format
+       "Mark with `%s' the currently selected thread.
+
+Operate on each message in the currently selected thread.  With
+optional BEG and END as points delimiting a region that
+encompasses multiple threads, operate on all those messages
+instead.
+
+With optional prefix argument (\\[universal-argument]) as UNTAG,
+reverse the application of the tags.
+
+This function advances to the next thread when finished."
+       tags)
+     (interactive (cons current-prefix-arg (notmuch-interactive-region)))
+     (when ,tags
+       (notmuch-search-tag
+        (notmuch-tag-change-list ,tags untag) beg end))
+     (when (eq beg end)
+       (notmuch-search-next-thread))))
+
+(prot-notmuch-search-tag-thread
+ prot-notmuch-search-delete-thread
+ prot-notmuch-mark-delete-tags)
+
+(prot-notmuch-search-tag-thread
+ prot-notmuch-search-flag-thread
+ prot-notmuch-mark-flag-tags)
+
+(prot-notmuch-search-tag-thread
+ prot-notmuch-search-spam-thread
+ prot-notmuch-mark-spam-tags)
+
+(defmacro prot-notmuch-show-tag-message (name tags)
+  "Produce NAME function parsing TAGS."
+  (declare (indent defun))
+  `(defun ,name (&optional untag)
+     ,(format
+       "Apply `%s' to message.
+
+With optional prefix argument (\\[universal-argument]) as UNTAG,
+reverse the application of the tags."
+       tags)
+     (interactive "P")
+     (when ,tags
+       (apply 'notmuch-show-tag-message
+              (notmuch-tag-change-list ,tags untag)))))
+
+(prot-notmuch-show-tag-message
+ prot-notmuch-show-delete-message
+ prot-notmuch-mark-delete-tags)
+
+(prot-notmuch-show-tag-message
+ prot-notmuch-show-flag-message
+ prot-notmuch-mark-flag-tags)
+
+(prot-notmuch-show-tag-message
+ prot-notmuch-show-spam-message
+ prot-notmuch-mark-spam-tags)
+
+(autoload 'notmuch-refresh-this-buffer "notmuch")
+(autoload 'notmuch-refresh-all-buffers "notmuch")
+
+(defun prot-notmuch-refresh-buffer (&optional arg)
+  "Run `notmuch-refresh-this-buffer'.
+With optional prefix ARG (\\[universal-argument]) call
+`notmuch-refresh-all-buffers'."
+  (interactive "P")
+  (if arg
+      (notmuch-refresh-all-buffers)
+    (notmuch-refresh-this-buffer)))
+
+;;;###autoload
+(defun prot-notmuch-delete-mail ()
+  "Permanently delete mail marked as `prot-notmuch-delete-mail'.
+Prompt for confirmation before carrying out the operation.
+
+Do not attempt to refresh the index.  This will be done upon the
+next invocation of 'notmuch new'."
+  (interactive)
+  (let* ((del-tag prot-notmuch-delete-tag)
+         (count
+          (string-to-number
+           (with-temp-buffer
+             (shell-command
+              (format "notmuch count tag:%s" prot-notmuch-delete-tag) t)
+             (buffer-substring-no-properties (point-min) (1- (point-max))))))
+         (mail (if (> count 1) "mails" "mail")))
+    (unless (> count 0)
+      (user-error "No mail marked as `%s'" del-tag))
+    (when (yes-or-no-p
+           (format "Delete %d %s marked as `%s'?" count mail del-tag))
+      (shell-command
+       (format "notmuch search --output=files --format=text0 tag:%s | xargs -r0 rm" del-tag)
+       t))))
+
+;;;; SourceHut-related setup
+
+(defconst prot-notmuch-patch-control-codes
+  '("PROPOSED" "NEEDS_REVISION" "SUPERSEDED" "APPROVED" "REJECTED" "APPLIED")
+  "Control codes for SourceHut patches.
+See `prot-notmuch-patch-add-email-control-code' for how to apply
+them.")
+
+(defun prot-notmuch--rx-in-sourcehut-mail (rx-group string)
+  "Return RX-GROUP of SourceHut mail in STRING."
+  (when (string-match-p "lists\\.sr\\.ht" string)
+    (string-clean-whitespace
+     (replace-regexp-in-string
+      ".*?[<]?\\(\\([-a-zA-Z0-9=._+~/]+\\)@\\(lists\\.sr\\.ht\\)\\)[>]?.*?"
+      (format "\\%s" rx-group) string))))
+
+(declare-function notmuch-show-get-header "notmuch-show" (header &optional props))
+(declare-function message-fetch-field "message" (header &optional first))
+
+(defun prot-notmuch--get-to-or-cc-header ()
+  "Get appropriate To or Cc header."
+  (cond
+   ((derived-mode-p 'notmuch-message-mode)
+    (concat (message-fetch-field "To") " " (message-fetch-field "Cc")))
+   ((derived-mode-p 'notmuch-show-mode)
+    (concat (notmuch-show-get-header :To) " " (notmuch-show-get-header :Cc)))))
+
+;; NOTE 2022-04-19: This assumes that we only have one list...  I think
+;; that is okay, but it might cause problems.
+(defun prot-notmuch--extract-sourcehut-mail (rx-group)
+  "Extract RX-GROUP from SourceHut mailing list address.
+1 is the full email address, 2 is the local part, while 3 is the
+domain."
+  (prot-notmuch--rx-in-sourcehut-mail
+   rx-group (prot-notmuch--get-to-or-cc-header)))
+
+(declare-function message-add-header "message" (&rest headers))
+
+;; Read: <https://man.sr.ht/lists.sr.ht/#email-controls>.
+;;;###autoload
+(defun prot-notmuch-patch-add-email-control-code (control-code)
+  "Add custom header for SourceHut email controls.
+The CONTROL-CODE is among `prot-notmuch-patch-control-codes'."
+  (interactive
+   (list (completing-read "Select control code: " prot-notmuch-patch-control-codes nil t)))
+  (if (member control-code prot-notmuch-patch-control-codes)
+      (unless (message-fetch-field "X-Sourcehut-Patchset-Update")
+        (message-add-header (format "X-Sourcehut-Patchset-Update: %s" control-code)))
+    (user-error "%s is not specified in `prot-notmuch-patch-control-codes'" control-code)))
+
+;;;###autoload
+(defun prot-notmuch-ask-sourcehut-control-code ()
+  "Use `prot-notmuch-patch-add-email-control-code' programmatically.
+Add this to `notmuch-mua-send-hook'."
+  (when-let* ((header (message-fetch-field "Subject"))
+              (subject (when (>= (length header) 6) (substring header 0 6)))
+              ((string= "[PATCH" subject)) ; Is [ always there?
+              ((prot-notmuch--extract-sourcehut-mail 1))
+              ((not (message-fetch-field "X-Sourcehut-Patchset-Update")))
+              ((y-or-n-p "Add control code for SourceHut PATCH?")))
+    (call-interactively #'prot-notmuch-patch-add-email-control-code)))
+
+;; NOTE 2022-04-19: Ideally we should be able to use the
+;; `notmuch-show-stash-mlarchive-link-alist' for
+;; `prot-notmuch-stash-sourcehut-link', but it assumes that the base URL
+;; is fixed for all message IDs, whereas those on SourceHut are not.
+
+(declare-function notmuch-show-get-message-id "notmuch-show" (&optional bare))
+(declare-function notmuch-show-message-top "notmuch-show")
+(declare-function notmuch-common-do-stash "notmuch-lib" (text))
+
+;;;###autoload
+(defun prot-notmuch-stash-sourcehut-link (&optional current)
+  "Stash web link to current SourceHut thread.
+With optional CURRENT argument, produce a link to the current
+message, else use the topmost message (start of the thread).
+
+Note that the topmost message is assumed to hold the id of the
+base URL, though this is not necessarily true."
+  (interactive "P")
+  (let* ((ml (prot-notmuch--extract-sourcehut-mail 2))
+         (base-id (save-excursion (goto-char (point-min))
+                                  (notmuch-show-message-top)
+                                  (notmuch-show-get-message-id t)))
+         (current-id (notmuch-show-get-message-id t)))
+    (notmuch-common-do-stash
+     (if current
+         (format "https://lists.sr.ht/%s/<%s>#<%s>" ml base-id current-id)
+       (format "https://lists.sr.ht/%s/<%s>" ml base-id)))))
+
+;;;###autoload
+(defun prot-notmuch-check-valid-sourcehut-email ()
+  "Check if SourceHut address is correct.
+Add this to `notmuch-mua-send-hook'."
+  (when-let* ((ml (prot-notmuch--extract-sourcehut-mail 1))
+              ((not (string-match-p "^\\(~\\|\\.\\)" ml)))
+              ((not (y-or-n-p "SourceHut address looks wrong.  Send anyway?"))))
+    (user-error "Incorrect SourceHut address")))
+
+(provide 'prot-notmuch)
+;;; prot-notmuch.el ends here
