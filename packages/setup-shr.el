bcef8e5d60374121df9a0857b1cb561346f261bd
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-07-21 14:21:46 +0300

    Update for webmode to render web stuff beautifully

---
+++ packages/setup-shr.el
@@ -0,0 +1,14 @@
+;;;; `shr' (Simple HTML Renderer)
+(use-package shr
+  :ensure nil
+  :defer t
+  :config
+  (setq shr-use-colors nil)             ; t is bad for accessibility
+  (setq shr-use-fonts nil)              ; t is superfluous, given `variable-pitch-mode'
+  (setq shr-max-image-proportion 0.6)
+  (setq shr-image-animate nil)          ; No GIFs, thank you!
+  (setq shr-width fill-column)
+  (setq shr-max-width fill-column)
+  (setq shr-discard-aria-hidden t)
+  (setq shr-fill-text nil)              ; Emacs 31
+  (setq shr-cookie-policy nil))
