64f5c44fcc1dc156adc52a883a90bb9612874807
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-07-22 12:04:01 +0300

    Add visual-fill-column

---
+++ packages/setup-visual-fill-column.el
@@ -0,0 +1,8 @@
+;; Visual fill column
+
+;; This is a replacement for auto-fill-mode so it provides a visual guide and
+;; not a hard breakline Useful for reading long lines as you would with
+;; auto-filled ones however I don't like using it for writing.
+
+(use-package visual-fill-column
+  :ensure t)
