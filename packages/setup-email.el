6d5427d861ccf2b40e2bbbebb9f56fc9adcfb350
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-07-22 12:02:26 +0300

    Not much indicator and a small sane default

--- packages/setup-email.el
+++ packages/setup-email.el
@@ -64,4 +64,29 @@
   (setq smtpmail-smtp-service 587)
   (setq smtpmail-stream-type 'starttls))
 
+(use-package notmuch-indicator
+  :ensure t
+  :after notmuch
+  :config
+  (setq notmuch-indicator-args
+        '(( :terms "tag:unread and tag:inbox"
+            :label "[U] ")
+          ( :terms "tag:unread and tag:package"
+            :label "[P] "))
+
+        notmuch-indicator-refresh-count (* 60 3)
+        notmuch-indicator-hide-empty-counters t
+        notmuch-indicator-force-refresh-commands '(notmuch-refresh-this-buffer))
+
+  ;; I control its placement myself.  See prot-emacs-modeline.el where
+  ;; I set the `mode-line-format'.
+  (setq notmuch-indicator-add-to-mode-line-misc-info nil)
+
+  (notmuch-indicator-mode 1))
+
+(defun sync-email ()
+  "Runs a script for syncing email"
+  (interactive)
+  (async-shell-command "/Users/ovistoica/.local/bin/sync-mail"))
+
 (provide 'setup-email)
