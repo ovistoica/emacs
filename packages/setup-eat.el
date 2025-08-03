d61806cedcf110b0d06db72e22d66f3533154b3a
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-08-03 08:30:56 +0300

    Fix eat setup

--- packages/setup-eat.el
+++ packages/setup-eat.el
@@ -3,11 +3,7 @@
 ;; Setup terminal emulator
 
 (use-package eat
-  :straight '(:type git :host codeberg :repo "akib/emacs-eat" :files ("*.el" ("term" "term/*.el") "*.texi"
-                                                                      "*.ti" ("terminfo/e" "terminfo/e/*")
-                                                                      ("terminfo/65" "terminfo/65/*")
-                                                                      ("integration" "integration/*")
-                                                                      (:exclude ".dir-locals.el" "*-tests.el")))
+  :ensure t
 
   :config
   ;; Unbind M-o from eat-mode keymaps
