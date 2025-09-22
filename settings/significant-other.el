f332bd5ac2646e6d384e53932506fdd094e30363
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-09-22 09:01:02 +0300

    Use remote significant other

--- settings/significant-other.el
+++ settings/significant-other.el
@@ -1,7 +1,7 @@
 ;;; Code:
 
 (use-package significant-other
-  :straight '(significant-other :type git :local-repo "~/workspace/significant-other")
+  :straight '(significant-other :type git :host github :repo "ovistoica/significant-other.el")
   :bind ("s-j" . significant-other-jump))
 
 (provide 'significant-other)
