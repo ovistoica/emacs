228661b6004ac0ab2bf21c3ea1e1b680a3742cba
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-10-17 09:40:01 +0300

    Convenience appearance packages

---
+++ packages/setup-appearance.el
@@ -0,0 +1,39 @@
+;; Packages that are related to appearance
+
+(use-package pulsar
+  :ensure t
+  :config
+  (setopt pulsar-pulse t
+          pulsar-delay 0.055
+          pulsar-iterations 10
+          pulsar-face 'pulsar-magenta
+          pulsar-highlight-face 'pulsar-cyan)
+
+  (pulsar-global-mode 1)
+  :hook
+  ;; There are convenience functions/commands which pulse the line using
+  ;; a specific colour: `pulsar-pulse-line-red' is one of them.
+  ((next-error . (pulsar-pulse-line-red pulsar-recenter-top pulsar-reveal-entry))
+   (minibuffer-setup . pulsar-pulse-line-red))
+  :bind
+  ;; pulsar does not define any key bindings.  This is just my personal
+  ;; preference.  Remember to read the manual on the matter.  Evaluate:
+  ;;
+  ;; (info "(elisp) Key Binding Conventions")
+  (("C-x l" . pulsar-pulse-line)        ; override `count-lines-page'
+   ("C-x L" . pulsar-highlight-dwim))) ; or use `pulsar-highlight-line'
+
+;;;; Lin
+;; Read the lin manual: <https://protesilaos.com/emacs/lin>.
+(use-package lin
+  :ensure t
+  :hook (after-init . lin-global-mode) ; applies to all `lin-mode-hooks'
+  :config
+  ;; You can use this to live update the face:
+  ;;
+  ;; (customize-set-variable 'lin-face 'lin-green)
+  ;;
+  ;; Or `setopt' on Emacs 29: (setopt lin-face 'lin-yellow)
+  ;;
+  ;; I still prefer `setq' for consistency.
+  (setq lin-face 'lin-magenta))
