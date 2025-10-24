60ef180cbaae2609764fd97658b753d1161da574
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-10-24 21:04:25 +0300

    Turn back ibuffer as C-x C-b

--- packages/setup-perspective.el
+++ packages/setup-perspective.el
@@ -6,8 +6,7 @@
 
 (use-package perspective
   :straight '(perspective :type git :host github :repo "nex3/perspective-el")
-  :bind (("C-x C-b" . persp-ibuffer)
-         ("C-x k" . persp-kill-buffer*)
+  :bind (("C-x k" . persp-kill-buffer*)
          ("C-x b" . persp-switch-to-buffer*))
   :init
   (setq persp-mode-prefix-key (kbd "C-x x"))
