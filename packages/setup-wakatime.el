8c346471fe143aa09f6d760b3ce24580f57e71bf
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-10-28 19:36:49 +0200

    Wakatime

--- packages/setup-wakatime.el
+++ packages/setup-wakatime.el
@@ -1,14 +1,14 @@
 ;; Waka time is a tracker of how much time you spend coding.
 ;; It's great to keep you honest
 
-(require 'private)
+(when (eq system-type 'darwin)
+  (require 'private)
 
-(use-package wakatime-mode
-  :ensure t
-  :diminish ""
-  :custom (setq wakatime-api-key my/secret-wakatime-api-key
-                wakatime-cli-path "~/.wakatime/wakatime-cli")
-  :init (global-wakatime-mode))
+  (use-package wakatime-mode
+    :diminish ""
+    :custom (setq wakatime-api-key my/secret-wakatime-api-key
+                  wakatime-cli-path "~/.wakatime/wakatime-cli")
+    :init (global-wakatime-mode)))
 
 
 (provide 'setup-wakatime)
