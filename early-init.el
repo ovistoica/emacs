274576e21d3d4cfffa79ae5f71c61692622a69ee
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-10-29 16:53:03 +0200

    early init

---
+++ early-init.el
@@ -0,0 +1,10 @@
+;;; early-init.el --- Early initialization -*- lexical-binding: t -*-
+;;
+;; This file is loaded before the package system and GUI is initialized.
+;;
+
+;; Prevent package.el from auto-loading packages before we initialize straight.el
+;; We still call (package-initialize) manually in settings/packages.el
+(setq package-enable-at-startup nil)
+
+;;; early-init.el ends here
