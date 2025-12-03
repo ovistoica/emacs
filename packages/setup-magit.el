c801ad6b4ad56e3660392ef0e3b0ca4ab7e4609f
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-12-03 08:04:18 +0200

    Add bitbucket PR function

--- packages/setup-magit.el
+++ packages/setup-magit.el
@@ -12,7 +12,8 @@
                       unstage-all-changes))
 
   :bind ((:map magit-status-mode-map
-               ("q" . magit-quit)))
+               ("q" . magit-quit)
+               ("C-c p r" . my/magit-create-bitbucket-pr)))
   :config
   (wrap-fullscreen magit-status)
   (wrap-fullscreen magit-init)
@@ -55,6 +56,32 @@ stored by magit-status fullscreen."
     (while (looking-at "#")
       (forward-line))
     (forward-line)))
+
+(defun my/magit-create-bitbucket-pr ()
+  "Generate and open Bitbucket PR URL for the current branch."
+  (interactive)
+  (let* ((current-branch (magit-get-current-branch))
+         (remote-url (magit-get "remote" "origin" "url"))
+         (repo-path (when remote-url
+                      (cond
+                       ;; HTTPS URL: https://bitbucket.org/workspace/repo.git
+                       ((string-match "https://bitbucket.org/\\(.+?\\)/\\(.+?\\)\\(?:\\.git\\)?$" remote-url)
+                        (format "%s/%s" (match-string 1 remote-url) (match-string 2 remote-url)))
+                       ;; SSH URL: git@bitbucket.org:workspace/repo.git
+                       ((string-match "git@bitbucket.org:\\(.+?\\)/\\(.+?\\)\\(?:\\.git\\)?$" remote-url)
+                        (format "%s/%s" (match-string 1 remote-url) (match-string 2 remote-url)))
+                       (t nil))))
+         (pr-url (when repo-path
+                   (format "https://bitbucket.org/%s/pull-requests/new?source=%s&t=1"
+                           repo-path current-branch))))
+    (cond
+     ((not current-branch)
+      (user-error "Not on a git branch"))
+     ((not repo-path)
+      (user-error "Could not parse Bitbucket repository from remote origin URL: %s" remote-url))
+     (t
+      (message "Opening PR for branch: %s" current-branch)
+      (browse-url pr-url)))))
 
 
 (provide 'setup-magit)
