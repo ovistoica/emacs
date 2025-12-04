9f03c527b8d11b672763b043321a1ae13137164f
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-12-04 08:53:45 +0200

    Enable elisp & elisp doc linting

--- packages/setup-linting.el
+++ packages/setup-linting.el
@@ -1,7 +1,15 @@
+;;; setup-linting --- Lint setup for my emacs -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Linting for various modes is done through flychecheck. LSP mode is
+;;; configured to use the same
+
+;;; Code:
 (use-package flycheck
   :diminish flycheck-mode
   :defer 2
-  :config (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc emacs-lisp html-tidy))
+  :config (setq-default flycheck-disabled-checkers '(html-tidy))
   :init
   ;; Eagerly re-check whenever there are errors.
   ;; When there are no errors, we're happy to wait for a save.
@@ -14,4 +22,5 @@
 
   (global-flycheck-mode))
 
-(provide 'setup-flycheck)
+(provide 'setup-linting)
+;;; setup-linting.el ends here
