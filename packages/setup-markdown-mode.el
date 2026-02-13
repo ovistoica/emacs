2bcc3b0d8f4d029d1ab7051ed0f953fe314e2088
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2026-02-13 07:40:42 +0200

    Fix markdown-mode workfing for ```clj blocks

--- packages/setup-markdown-mode.el
+++ packages/setup-markdown-mode.el
@@ -1,7 +1,17 @@
-(use-package markdown
-  :ensure nil
-  :defer t
+;;; setup-markdown-mode --- Markdown mode configuration -*- lexical-binding: t -*-
+
+;;; Commentary:
+;; Native code block fontification with clj and other modes
+
+;;; Code:
+
+(use-package markdown-mode
+  :ensure t
   :custom
-  (markdown-fontify-code-blocks-natively t))
+  (markdown-fontify-code-blocks-natively t)
+  :config
+  ;; Add 'clj' as an alias for 'clojure' in markdown code blocks
+  (add-to-list 'markdown-code-lang-modes '("clj" . clojure-mode)))
 
 (provide 'setup-markdown-mode)
+;;; setup-markdown-mode.el ends here
