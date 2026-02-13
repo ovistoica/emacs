5581e94f924f41121944586c5fe3c89fa207f73c
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2026-02-13 07:37:40 +0200

    Add themes used

---
+++ packages/setup-themes.el
@@ -0,0 +1,18 @@
+;;; setup-themes --- Theme packages -*- lexical-binding: t -*-
+
+;;; Commentary:
+;; Install modus-themes and doom-themes for use with omarchy theme mapping.
+
+;;; Code:
+
+(use-package modus-themes
+  :ensure t)
+
+(use-package doom-themes
+  :ensure t)
+
+(use-package catppuccin-theme
+  :ensure t)
+
+(provide 'setup-themes)
+;;; setup-themes.el ends here
