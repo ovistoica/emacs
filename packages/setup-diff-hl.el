9bcb7d24a56f45de786b116209c32d71ffb6625a
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2026-04-20 09:08:53 +0300

    Fix diff-hl git gutter starting on emacs start

--- packages/setup-diff-hl.el
+++ packages/setup-diff-hl.el
@@ -4,10 +4,9 @@
 ;; as the "gutter"), allows you to jump between and revert them selectively.
 
 (use-package diff-hl
-  :defer t
-  :config
-  (global-diff-hl-mode)
-  (add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh)
-  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
+  :hook ((after-init . global-diff-hl-mode)
+         (magit-pre-refresh . diff-hl-magit-pre-refresh)
+         (magit-post-refresh . diff-hl-magit-post-refresh)))
 
 (provide 'setup-diff-hl)
+;;; setup-diff-hl.el ends here
