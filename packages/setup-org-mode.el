a1e80bb8e8f18614df8c6f9d7e4b8da684ceaa22
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2026-07-14 08:24:14 +0300

    Update org mode keywords

--- packages/setup-org-mode.el
+++ packages/setup-org-mode.el
@@ -40,6 +40,10 @@
     "Face for the REVIEW Org keyword.")
   (defface my/org-todo-pending-qa '((t :weight bold))
     "Face for the PENDING_QA Org keyword.")
+  (defface my/org-todo-next '((t :weight bold))
+    "Face for the NEXT Org keyword.")
+  (defface my/org-todo-proj '((t :weight bold))
+    "Face for the PROJ Org keyword.")
   (defface my/org-todo-wait '((t :weight bold))
     "Face for the WAIT Org keyword.")
   (defface my/org-todo-event '((t :weight bold))
@@ -54,6 +58,8 @@
                       (my/org-todo-inprogress . yellow-warmer)
                       (my/org-todo-review     . blue)
                       (my/org-todo-pending-qa . cyan)
+                      (my/org-todo-next       . red-warmer)
+                      (my/org-todo-proj       . magenta)
                       (my/org-todo-wait       . yellow)
                       (my/org-todo-event      . blue-warmer)
                       (my/org-todo-done       . bg-added-fringe)))
@@ -62,8 +68,8 @@
 
   :custom
   (org-todo-keywords
-   '((sequence "TODO(t)" "INPROGRESS(i)" "REVIEW(r)" "PENDING_QA(p)"
-               "WAIT(w)" "EVENT(e)" "|" "DONE(d)" "NOTABUG(n)")))
+   '((sequence "TODO(t)" "INPROGRESS(i)" "REVIEW(r)" "PENDING_QA(q)" "NEXT(n)"
+               "PROJ(p)" "WAIT(w)" "EVENT(e)" "|" "DONE(d)" "CANCELED(c)")))
 
 
   (org-todo-keyword-faces
@@ -71,6 +77,8 @@
      ("INPROGRESS" . my/org-todo-inprogress)
      ("REVIEW" . my/org-todo-review)
      ("PENDING_QA" . my/org-todo-pending-qa)
+     ("NEXT" . my/org-todo-next)
+     ("PROJ" . my/org-todo-proj)
      ("WAIT" . my/org-todo-wait)
      ("EVENT" . my/org-todo-event)
      ("DONE" . my/org-todo-done)))
@@ -112,8 +120,10 @@
           ;;  "* I am %?\n  %U\n\nCurrent affirmations:\n- I am a clojure startup owner of 1M ARR\n- I own a medium house in the country-side \n- I am a man of 70kgs\n- I am a husband of a happy family" :empty-lines 1)
           ("b" "Bookmark" entry (file+headline "~/Dropbox/org/bookmarks.org" "Bookmarks")
            "* %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n\n" :empty-lines 1)
-          ;; ("t" "Todo" entry (file+headline "~/Dropbox/org/working-memory.org" "INBOX")
-          ;;  "* TODO %?\n  %i\n  %a")
+          ("t" "Todo" entry (file+headline "~/Dropbox/org/working-memory.org" "INBOX")
+           "* TODO %?\n  %i\n  %a")
+          ("w" "Work Todo" entry (file+headline "~/Dropbox/org/master-list.org" "INBOX")
+           "* TODO %?\n  %i\n  %a")
           ;; ("j" "Journal" entry  (file+olp+datetree "~/Dropbox/org/journal.org")
           ;;  "* %?\nEntered on %U\n  %i\n ")
           ("u" "Eng Update" entry  (file+olp+datetree "~/Dropbox/org/engineering-update.org")
@@ -181,13 +191,13 @@ installed."
 ;;; Auto-sort subtree by TODO state after state changes
 
 (defcustom my/org-todo-sort-order
-  '("INPROGRESS" "TODO" "WAIT" "EVENT" "REVIEW" "DONE" "NOTABUG")
+  '("NEXT" "INPROGRESS" "TODO" "WAIT" "EVENT" "REVIEW" "DONE" "CANCELED")
   "Sort priority for TODO keywords; earlier entries sort first.
 Keywords absent from this list sink to the bottom."
   :type '(repeat string)
   :group 'org)
 ;; Ensure the order is always applied, even if defcustom skipped the init value
-(setq my/org-todo-sort-order '("INPROGRESS" "TODO" "WAIT" "EVENT" "REVIEW" "DONE" "NOTABUG"))
+(setq my/org-todo-sort-order '("INPROGRESS" "TODO" "WAIT" "EVENT" "REVIEW" "DONE" "CANCELED"))
 
 (defun my/org-todo-sort-key ()
   "Return a numeric sort key for the current heading's TODO state."
